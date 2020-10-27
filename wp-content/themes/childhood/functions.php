c6851c97f2872dd578c2b9ed168d9a26a6e6a491
binom989 <binom9@yandex.ru>
2020-10-27 16:28:38 +0400

    27.10

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -1,8 +1,14 @@
 <?php
+add_action('wp_enqueue_scripts', 'childhood_styles');
 add_action('wp_enqueue_scripts', 'childhood_scripts');
 
+function childhood_styles() {
+    wp_enqueue_style( 'childhood_style', get_stylesheet_uri() ); // подключение стилистического файла
+};
+
+
 function childhood_scripts() {
-    wp_enqueue_style( 'childhood_style', get_stylesheet_uri() );
+    wp_enqueue_script( 'childhood_scripts',  get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);  // подключение скрипта из директории assets
 };
 
 ?>
