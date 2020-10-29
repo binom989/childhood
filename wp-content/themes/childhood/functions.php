82429554715d95a3927b5c720cfcdebfe34019cf
binom989 <binom9@yandex.ru>
2020-10-29 15:51:49 +0400

    29.10

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -11,4 +11,6 @@ function childhood_scripts() {
     wp_enqueue_script( 'childhood_scripts',  get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);  // подключение скрипта из директории assets
 };
 
+add_theme_support( 'custom-logo' );  // добавляем возможность ручного подключения логотипа
+
 ?>
